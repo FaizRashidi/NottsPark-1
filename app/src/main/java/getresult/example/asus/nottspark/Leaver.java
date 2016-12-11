67080116ea069db251fd9a0dba68fd81ce6b0155
vivian.yeoh <khcy6yhw@nottingham.edu.my>
2016-12-11 18:24:21 +0800

    update design

--- app/src/main/java/getresult/example/asus/nottspark/Leaver.java
+++ app/src/main/java/getresult/example/asus/nottspark/Leaver.java
@@ -1,8 +1,11 @@
 package getresult.example.asus.nottspark;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
 public class Leaver extends AppCompatActivity {
 
@@ -14,6 +17,18 @@ public class Leaver extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Button mParkerButton = (Button) findViewById(R.id.btnSearchParker);
+        mParkerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                searchParker(view);
+            }
+        });
     }
-}
+
+
+    public void searchParker(View v) {
+        Intent intent = new Intent(this, ParkerFound.class);
+        startActivity(intent);
+    }}
 
--- app/src/main/java/getresult/example/asus/nottspark/Parker.java
+++ app/src/main/java/getresult/example/asus/nottspark/Parker.java
@@ -1,10 +1,12 @@
 package getresult.example.asus.nottspark;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
 public class Parker extends AppCompatActivity {
 
@@ -16,6 +18,20 @@ public class Parker extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Button mLeaverButton = (Button) findViewById(R.id.btnSearchLeaver);
+        mLeaverButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                searchLeaver(view);
+            }
+        });
+    }
+
+
+    public void searchLeaver(View v) {
+        Intent intent = new Intent(this, LeaverFound.class);
+        startActivity(intent);
     }
 }
 
